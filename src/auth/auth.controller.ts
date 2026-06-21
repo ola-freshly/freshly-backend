b26cac6bba7dd35b0f753e6726b98de480168ece
hieuvu121 <hieubeobilabi@gmail.com>
2026-06-21 23:56:45 +1000

    add interceptor

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,12 +7,14 @@ import {
   Res,
   HttpCode,
   HttpStatus,
+  Request,
 } from '@nestjs/common';
 import type { Response } from 'express';
 import { ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -21,21 +23,30 @@ export class AuthController {
     private readonly config: ConfigService,
   ) {}
 
+  @Public()
   @Post('register')
   @HttpCode(HttpStatus.CREATED)
   register(@Body() dto: RegisterDto) {
     return this.authService.register(dto);
   }
 
+  @Public()
   @Get('verify-email')
   async verifyEmail(@Query('token') token: string, @Res() res: Response) {
     await this.authService.verifyEmail(token);
     return res.redirect(`${this.config.get('FRONTEND_URL')}/login?verified=true`);
   }
 
+  @Public()
   @Post('login')
   @HttpCode(HttpStatus.OK)
   login(@Body() dto: LoginDto) {
     return this.authService.login(dto);
+  }
+
+  //test jwt validation
+  @Get('me')
+  me(@Request() req: { user: { id: string; email: string } }) {
+    return req.user;
   }
 }
