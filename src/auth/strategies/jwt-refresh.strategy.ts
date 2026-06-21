4c7320b6d0a0b108c4cf47d65461893f49208f22
hieuvu121 <hieubeobilabi@gmail.com>
2026-06-21 23:26:33 +1000

    FRES-41 Login API

---
+++ src/auth/strategies/jwt-refresh.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
+
+@Injectable()
+export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromBodyField('refreshToken'),
+      secretOrKey: config.get<string>('JWT_REFRESH_SECRET') as string,
+      passReqToCallback: true as const,
+    } as any);
+  }
+
+  validate(req: Request, payload: { sub: string }) {
+    return { id: payload.sub, refreshToken: req.body.refreshToken as string };
+  }
+}
