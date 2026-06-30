1f31e6780b051b0c1d40c6e2eebee1381a7fba88
Khang Le <lenguyenkhang102002@gmail.com>
2026-06-30 16:46:32 +0700

    FRES-44: main.ts formatted

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      process.env.FRONTEND_URL ?? 'http://localhost:8081',
-    ],
+    origin: [process.env.FRONTEND_URL ?? 'http://localhost:8081'],
     credentials: true,
   });
   app.useGlobalPipes(
