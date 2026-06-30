2fdbfbbaa33fad728a18054b76419419a89e82bf
Khang Le <lenguyenkhang102002@gmail.com>
2026-06-30 16:07:29 +0700

    FRES-44: enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      process.env.FRONTEND_URL ?? 'http://localhost:8081',
+    ],
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
