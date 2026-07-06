178a1112fddc5a952d212ce20f5a36f29eade2bc
hieuvu121 <hieubeobilabi@gmail.com>
2026-07-06 18:01:01 +1000

    Fix test files

--- src/classes/pantry-items/pantry-items.controller.spec.ts
+++ src/classes/pantry-items/pantry-items.controller.spec.ts
@@ -2,13 +2,22 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PantryItemsController } from './pantry-items.controller';
 import { PantryItemsService } from './pantry-items.service';
 
+const mockPantryItemsService = {
+  create: jest.fn(),
+  findAll: jest.fn(),
+  findOne: jest.fn(),
+  update: jest.fn(),
+  remove: jest.fn(),
+  scanImage: jest.fn(),
+};
+
 describe('PantryItemsController', () => {
   let controller: PantryItemsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PantryItemsController],
-      providers: [PantryItemsService],
+      providers: [{ provide: PantryItemsService, useValue: mockPantryItemsService }],
     }).compile();
 
     controller = module.get<PantryItemsController>(PantryItemsController);
